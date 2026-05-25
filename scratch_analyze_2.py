288a4d50de1bc9949f4bcacf0753b345e80b37b2
unknown <ramyaworkac@gmail.com>
2026-05-25 08:01:10 +0530

    UI Standardization: Update header layout across master and expense modules

--- scratch_analyze_2.py
+++
@@ -1,6 +0,0 @@
-with open('d:/AuraaZenAIProject/abproject/templates/purchase_bill.html', 'r', encoding='utf-8') as f:
-    c = f.read()
-
-m2 = c.find('id="newCashPurchaseModal"')
-print('pbViewMenu inside newCashPurchaseModal:', c.find('id="pbViewMenu"', m2) != -1)
-print('pbViewQuickBtn inside newCashPurchaseModal:', c.find('id="pbViewQuickBtn"', m2) != -1)
